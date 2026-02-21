6d51c01107a0a5620f2ebadb3287343253f2600f
Berke Şen <berke67esen@gmail.com>
2026-02-21 19:59:09 +0300

    Delete GymPlanner_api - Copy directory

--- GymPlanner_api - Copy/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-router.post('/register', authController.register);
-router.post('/login', authController.login);
-router.put('/update-profile/:id', authController.updateProfile);
-
-module.exports = router;
--- GymPlanner_api - Copy/routes/logRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const logController = require('../controllers/logController');
-const checkAuth = require('../middleware/authMiddleware');
-
-router.post('/', checkAuth, logController.createLog);
-router.get('/history', checkAuth, logController.getLogsByExercise);
-router.delete('/:id', checkAuth, logController.deleteLog);
-
-module.exports = router;
